d4a6f25382c6e28147180b459b7cc5b83e303a8d
Jenny Watts <jennie350@gmail.com>
2021-11-15 18:25:17 +0800

    Added JS
    added search form, change temp, city name, feels like, humidty + windspeed

---
+++ src/app.js
@@ -0,0 +1,41 @@
+let now = new Date();
+let days = [
+  `Sunday`,
+  `Monday`,
+  `Tuesday`,
+  `Wednesday`,
+  `Thursday`,
+  `Friday`,
+  `Saturday`,
+];
+let day = days[now.getDay()];
+let currentDay = document.querySelector("#day");
+let time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+currentDay.innerHTML = `${day} ${time}`;
+
+function showWeather(response) {
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector(".temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(".current-weather").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#windspeed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-search-input").value;
+  let apiKey = `e26555d3812486da4794499d94833f23`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showWeather);
+}
+
+let form = document.querySelector("#city-search-form");
+form.addEventListener("submit", submitSearch);
